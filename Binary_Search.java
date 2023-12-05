9e4bf1a5d196d90bd854c1b735ce9b83b9906334
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-12-05 20:32:54 +0530

    Create Binary_Search.java

---
+++ Binary_Search.java
@@ -0,0 +1,13 @@
+class Solution {
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        for(int i=0;i<n;i++){
+            if(arr[i]==k){
+                return i;
+            }
+            
+        }
+        return -1;
+        
+    }
+}
